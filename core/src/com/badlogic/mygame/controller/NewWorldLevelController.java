a830cd6cb991cb111d84ba9da89a49f2b47a64e5
Juunicacio <juhnicacio.bio@gmail.com>
2022-03-05 19:41:15 +0100

    Added depth from y.

--- core/src/com/badlogic/mygame/controller/NewWorldLevelController.java
+++ core/src/com/badlogic/mygame/controller/NewWorldLevelController.java
@@ -2,6 +2,7 @@ package com.badlogic.mygame.controller;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
@@ -18,11 +19,13 @@ public class NewWorldLevelController {
     // load map
     private static MyTiledMap myTiledMap;
     // draw sprites
-    public static Batch spriteBatch;
+    public static Batch batch;
     public static World world;
     private static Array<Body> bodiesInWorld; //Every time that a body is added, add it to a list as well
     private static Box2DDebugRenderer box2DDebugRenderer;
     //public static final float UNIT_SCALE = 1/32f;
+
+    static TextureRegion grassTr;
 
     public static final float UNIT_SCALE = 1f;
     public static boolean clearLevel = false;
@@ -30,6 +33,7 @@ public class NewWorldLevelController {
     //14,14,32,32
     public static final int MAPWIDTH = 14;
     public static final int MAPHEIGHT = 14;
+
     public static final int TILEWIDTH = 32;
     public static final int TILEHEIGHT = 32;
 
@@ -41,12 +45,14 @@ public class NewWorldLevelController {
         MyTiledMap myTiledMap = new MyTiledMap();
         renderer = myTiledMap.setUpMap();
 
+        grassTr = myTiledMap.getTextureRegionOfTile(48, 60, 64);
+
         world = new World(new Vector2(0,0), true); // this game doesn't need gravity
 
         box2DDebugRenderer = new Box2DDebugRenderer();
         box2DDebugRenderer.setDrawBodies(true);
 
-        spriteBatch = renderer.getBatch();
+        batch = renderer.getBatch();
     }
 
     /**
@@ -55,20 +61,25 @@ public class NewWorldLevelController {
 
     public static void update(float deltaTime, OrthographicCamera camera){
         world.step(1/60f, 6, 2);
-        spriteBatch.setProjectionMatrix(camera.combined);
+        batch.setProjectionMatrix(camera.combined);
         renderer.setView(camera);
     }
 
     public static void draw(OrthographicCamera camera){
-        //Cannot read field "ground" because "com.badlogic.mygame.controller.NewWorldLevelController.myTiledMap" is null
-        //renderer.render(myTiledMap.getGround());
-        //renderer.render();
-        renderer.render();
-        spriteBatch.begin();
-        // here comes the player
-        PlayerController.draw(spriteBatch); // here comes the player
-        spriteBatch.end();
-        //renderer.render(tileMapHelper.aboveCharLayersIndices); // put here clouds
+
+        batch.begin();
+        if(PlayerController.player.getPosition().y>grassTr.getRegionY())
+        {
+            System.out.println("Player Y is higher than Tile Y");
+            // here comes the player
+            PlayerController.draw(batch); // here comes the player
+            batch.draw(grassTr, grassTr.getRegionX(), grassTr.getRegionY(), grassTr.getRegionWidth(), grassTr.getRegionHeight());
+        }
+        else{
+            batch.draw(grassTr, grassTr.getRegionX(), grassTr.getRegionY(), grassTr.getRegionWidth(), grassTr.getRegionHeight());
+            PlayerController.draw(batch); // here comes the player
+        }
+        batch.end();
 
         box2DDebugRenderer.render(world, camera.combined);
     }
--- core/src/com/badlogic/mygame/helper/MyTiledMap.java
+++ core/src/com/badlogic/mygame/helper/MyTiledMap.java
@@ -1,7 +1,9 @@
 package com.badlogic.mygame.helper;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.MapLayers;
@@ -12,87 +14,132 @@ import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
 import com.badlogic.mygame.controller.NewWorldLevelController;
+import com.badlogic.mygame.controller.PlayerController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MyTiledMap extends ApplicationAdapter {
-    String pathToFile = "Land.png";
+    private String pathToFile = "Land.png";
 
     public static TiledMap map;
     static TiledMapTileLayer tileLayer;
-    static MapLayers layers;
+    //static MapLayers layers;
     TiledMapTileSet tileSet;
     //TiledMapTile tile;
-    int tileId=0;
-    Texture png;
-    TextureRegion tileInPng;
+    int tileId;
+    //********* GRASS
+    private int GRASSID = 48;
+    private int GRASSREGIONX = 60;
+    private int GRASSREGIONY = 64;
+    //*********
+
+    private Texture png;
+    TextureRegion tilesTR;
+    TextureRegion [] tilesInPng;
+    TextureRegion[][] tilesMatrix;
     public int[] ground; // for ex. ground and water - Bottom Layer
     MapLayer layer;
+
+    // array list of TextureRegion
+    private List<TextureRegion> myTilesList = new ArrayList<>();
 
     public OrthogonalTiledMapRenderer setUpMap(){
         map = new TiledMap();
         tileLayer = new TiledMapTileLayer(NewWorldLevelController.MAPWIDTH, NewWorldLevelController.MAPHEIGHT, NewWorldLevelController.TILEWIDTH, NewWorldLevelController.TILEHEIGHT);
         map.getLayers().add(tileLayer);
-
         png = new Texture(pathToFile);
-        tileInPng = new TextureRegion(png, 32, 32);
-
-        //TILE DEFINITION ###########
-        TiledMapTile tile = new StaticTiledMapTile(tileInPng);
-        //###########################
-
-        //TiledMapTileLayer.Cell cell = new TiledMapTileLayer.Cell();
-        //cell.setTile(tile);
-
-        tileSet = new TiledMapTileSet();
-        tileSet.putTile(0, tile);
-
-        int id=0;
-        for (int row = 0; row < NewWorldLevelController.MAPHEIGHT; row++) {
-            for (int col = 0; col < NewWorldLevelController.MAPWIDTH; col++) {
-
-                TiledMapTileLayer.Cell cell = new TiledMapTileLayer.Cell();
-                //tileSet.putTile(tileId, tile);
-                cell.setTile(tileSet.getTile(id));
-                //id++;
-
-                if(tileSet.getTile(id) != null) {
-                    System.out.println("1.*********************"); //id=0 is not null
-                    tileSet.getTile(id).getProperties(); //com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile@a82cf78
-                    tileLayer.setCell(col, row, cell);
-                }
-                System.out.println(tileSet.getTile(id)); //com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile@122648c9
-
-                System.out.println("tileLayer.toString(): " + tileLayer.toString()); //com.badlogic.gdx.maps.tiled.TiledMapTileLayer@2d436cc
-                System.out.println("tile: " + tile); //com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile@122648c9
-                System.out.println("tileSet.getName(): " + tileSet.getName()); //tileSet.getName(): null
-                System.out.println("tile.getProperties(): " + tile.getProperties()); //com.badlogic.gdx.maps.MapProperties@5f442ac3
-                id++;
-
-                //tileLayer.setCell(col, row, cell);
-
-                if(tileSet.getTile(id) != null) {
-                    System.out.println("2.*********************");
-
-                }
-                //System.out.printf("tileSet.getTile(%d): %d\n", id , tileSet.getTile(id)); //null
-                //id++;
-            }
-        }
-        // Reading Map Layers
-        MapLayers layers = map.getLayers();
-        System.out.println("layers: " + map.getLayers());
-        System.out.println("layers: " + map.getLayers().get(0)); //tileLayer
-        System.out.println("layers: " + map.getLayers().get(0));
-        //layers = map.getLayers();
-        //layer = map.getLayers().get(0);
-        //ground = new int[]{
-                //0
-        //};
-        //ground = new int[]{ 1,2,3,4,5,6,7,8,9,10 };
-        //map.getLayers().get(0); // I added just one layer
+        myTilesList = createMyTilesList(png);
         return new OrthogonalTiledMapRenderer(map, NewWorldLevelController.UNIT_SCALE);
     }
 
-    public int[] getGround() {
-        return ground;
+    public List<TextureRegion> createMyTilesList(Texture png){
+        //tileInPng = new TextureRegion(png, 32, 32);
+        //#******** tiles count in png file ********************************************
+        tilesMatrix = TextureRegion.split(png, NewWorldLevelController.TILEWIDTH, NewWorldLevelController.TILEHEIGHT);
+        System.out.println("tilesInPng.length : "+ tilesMatrix.length); //23
+        int matrixLength = tilesMatrix.length; //23
+
+        for(int i =0; i<matrixLength;i++){
+            tilesInPng = new TextureRegion[matrixLength];
+            for (TextureRegion[] tilesInPng:tilesMatrix) {
+                /*System.out.println("tilesInPng[0].getRegionX();: " + tilesInPng[i].getRegionX());
+                System.out.println("tilesInPng[0].getRegionY();: " + tilesInPng[i].getRegionY());
+                System.out.println("tilesInPng[0].getTexture();: " + tilesInPng[i].getTexture()); //Land.png
+                System.out.println("tilesInPng[0].getRegionWidth();: " + tilesInPng[i].getRegionWidth()); //32
+                System.out.println("tilesInPng[0].getRegionHeight();: " + tilesInPng[i].getRegionHeight()); //32*/
+                tilesTR = new TextureRegion(tilesInPng[i]);
+                myTilesList.add(tilesTR);
+            }
+        }
+        for(int i=0;i<myTilesList.size();i++)
+        {
+            System.out.println("myTilesList.get(i): " + myTilesList.get(i));
+            //System.out.println("i: " + i); // 528 total
+        }
+        return myTilesList;
     }
+
+    public TextureRegion getTextureRegionOfTile(int tileId, int regionX, int regionY){
+        int id=tileId; // 528 tiles in total // ABOVE 46
+        TextureRegion tR = myTilesList.get(id);
+        //TiledMapTile tile = new StaticTiledMapTile(tR);
+        //tR.getRegionX();
+        //tR.getRegionY();
+        System.out.println("tR.getRegionX() : " + tR.getRegionX());
+        System.out.println("tR.getRegionY() : " + tR.getRegionY());
+        tR.setRegionX(regionX); // 60
+        tR.setRegionY(regionY); // 64 the same
+        /*FOR GRASS: id=49 / 48, tR.setRegionX(60)*/
+
+        //createCell(tileId, tR);
+
+        return tR;
+        /*tileSet = new TiledMapTileSet();
+        tileSet.putTile(id, tile); //undo
+        TiledMapTileLayer.Cell cell = new TiledMapTileLayer.Cell();
+        cell.setTile(tileSet.getTile(id));
+        return cell;*/
+    }
+
+    public void createCell(int tileId, TextureRegion tR){
+        //******************************************************************************
+        //TextureRegion grassTr = getTextureRegionOfTile(grassId, grassRegionX, grassRegionY);
+        //******************************************************************************
+
+        TiledMapTile tile = new StaticTiledMapTile(tR);
+        tileSet = new TiledMapTileSet();
+        tileSet.putTile(tileId, tile);
+
+        TiledMapTileLayer.Cell cell = new TiledMapTileLayer.Cell();
+        cell.setTile(tileSet.getTile(tileId));
+
+        for (int col = 0; col < NewWorldLevelController.MAPWIDTH; col++) {
+            tileLayer.setCell(col, 0, cell);
+        }
+        /*// Reading Map Layers
+        MapLayers layers = map.getLayers();
+        System.out.println("layers: " + map.getLayers());
+        System.out.println("layers: " + map.getLayers().get(0)); //tileLayer
+        System.out.println("layers: " + map.getLayers().get(0));*/
+    }
+
+    public void positionYControl(TextureRegion tR, Batch batch){
+        batch.begin();
+        if(PlayerController.player.getPosition().y>tR.getRegionY())
+        {
+            System.out.println("Player Y is higher than Tile Y");
+            // here comes the player
+            PlayerController.draw(batch); // here comes the player
+            batch.draw(tR, tR.getRegionX(), tR.getRegionY(), tR.getRegionWidth(), tR.getRegionHeight());
+        }
+        else{
+            batch.draw(tR, tR.getRegionX(), tR.getRegionY(), tR.getRegionWidth(), tR.getRegionHeight());
+            PlayerController.draw(batch); // here comes the player
+        }
+        batch.end();
+    }
+
+
 }
